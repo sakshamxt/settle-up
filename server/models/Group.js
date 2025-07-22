d71f9d19ca3daa17f6acd08e866a9051b3cb61a0
Saksham Tyagi <saksham47t@gmail.com>
2025-07-22 11:09:46 +0530

    updated backend

---
+++ server/models/Group.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const GroupSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  description: {
+    type: String,
+    required: false,
+    trim: true,
+  },
+  members: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+  }],
+  createdBy: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'User',
+    required: true,
+  }
+}, {
+  timestamps: true,
+});
+
+const Group = mongoose.model("Group", GroupSchema);
+
+export default Group;
