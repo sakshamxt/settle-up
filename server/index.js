cc5a17f51a004a8d191a647ad74946e5e6546f0a
Saksham Tyagi <saksham@Sakshams-Mac-mini.local>
2025-07-22 10:15:11 +0530

    initialised backend

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the server!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
